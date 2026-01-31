c960139f6edf83b0148b4051c86c810cff9b3dea
kowsikadevi21082006 <deva36rp@gmail.com>
2026-01-31 14:21:09 +0530

    Flutter app connected to firebase

--- artisan_app/lib/main.dart
+++ artisan_app/lib/main.dart
@@ -1,20 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'core/theme.dart';
-import 'features/auth/splash_screen.dart';
-import 'features/auth/welcome_screen.dart';
-import 'features/auth/login_screen.dart';
-import 'features/auth/signup_screen.dart';
-import 'features/auth/role_selection_screen.dart';
-import 'features/artisan/artisan_profile_setup.dart';
-import 'features/artisan/artisan_dashboard.dart';
-import 'features/artisan/product_management.dart';
 import 'features/customer/customer_home.dart';
 import 'features/customer/order_tracking.dart';
 import 'features/customer/product_details.dart';
 import 'features/common/notifications_screen.dart';
 import 'features/common/profile_screen.dart';
+import 'features/artisan/artisan_profile_setup.dart';
+import 'features/artisan/artisan_dashboard.dart';
+import 'features/artisan/product_management.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LocalArtisanApp());
 }
 
@@ -27,14 +28,8 @@ class LocalArtisanApp extends StatelessWidget {
       title: 'Local Artisan Digital Storefront',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: '/',
+      initialRoute: '/customer-home',
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/signup': (context) => const SignupScreen(),
-        '/role-selection': (context) => const RoleSelectionScreen(),
-        
         // Artisan Flow
         '/artisan-setup': (context) => const ArtisanProfileSetup(),
         '/artisan-dashboard': (context) => const ArtisanDashboard(),
